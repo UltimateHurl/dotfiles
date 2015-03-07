dad9291f22ffa762a8bd507cc2f05a8918bc7dd1
Eoin Hurrell <ultimatehurl@gmail.com>
2015-03-07 07:47:27 +0000

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# Lein install
-sudo -v
-wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
-mv lein /usr/local/bin
-chmod a+x /usr/local/bin/lein
-
 # Anaconda install
 http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.1.0-MacOSX-x86_64.sh
 bash Anaconda-2.1.0-MacOSX-x86_64.sh
