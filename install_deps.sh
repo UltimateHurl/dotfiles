126d5250abca897de0556f8b15caae3f890dbdca
Eoin Hurrell <eoinhurrell@users.noreply.github.com>
2019-10-24 14:08:48 +0100

    Delete install_deps.sh

--- install_deps.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-# Anaconda install
-http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.1.0-MacOSX-x86_64.sh
-bash Anaconda-2.1.0-MacOSX-x86_64.sh
